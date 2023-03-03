f7608ad70d118fdb9774fbe94c8a2e05ffe79e76
0xol <86998622+0xol@users.noreply.github.com>
2023-03-03 22:46:13 +1000

    python arch build systems

--- make.py
+++ make.py
@@ -1,13 +1,12 @@
 import config
 import os
 import subprocess
+import importlib
 
 cpu_count = os.cpu_count()
 
 def system(command):
     subprocess.run(command, shell=True, check=True)
 
-if config.arch == "amd64":
-    system(f"cd arch/x86 && make -j {cpu_count}")
-    system(f"cd arch/x86/amd64 && make -j {cpu_count}")
-else: system(f"cd arch/{config.arch} && make -j {cpu_count}")
+if config.arch == "amd64": arch_module = importlib.import_module("arch.x86.build")
+else: arch_module = importlib.import_module(f"arch.{config.arch}.build")
