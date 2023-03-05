c7e1dc9b8e228655377cec4caaf3d1b3897fdd08
0xol <86998622+0xol@users.noreply.github.com>
2023-03-05 19:00:42 +1000

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -3,6 +3,7 @@
 sudo apt install libgmp-dev libmpfr-dev libmpc-dev
 
 cd ~
+mkdir toolchain
 mkdir build-gcc
 mkdir build-bin
 wget https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.xz -O binutils.tar.xz
@@ -14,11 +15,13 @@ mv gcc-12.2.0 gcc
 mv binutils-2.40 bin
 
 cd build-bin
-../bin/configure --prefix=/usr --target=x86_64-none-elf --with-sysroot --disable-nls --disable-werror
+../bin/configure --prefix=/home/runner/toolchain --target=x86_64-none-elf --with-sysroot --disable-nls --disable-werror
 make -j 2
 sudo make install -j 2
 
 cd ../build-gcc
-../gcc/configure --prefix=/usr --target=x86_64-none-elf --disable-nls --enable-languages=c --without-headers
+../gcc/configure --prefix=/home/runner/toolchain --target=x86_64-none-elf --disable-nls --enable-languages=c --without-headers
 make -j 2
 sudo make install -j 2
+
+export PATH="$HOME/toolchain/bin:$PATH"
