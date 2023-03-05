7076a65f2566225d83093889c72197c78309e951
0xol <86998622+0xol@users.noreply.github.com>
2023-03-05 17:15:50 +1000

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -7,3 +7,6 @@ wget https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.xz -O binutils.tar.xz
 wget https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz -O gcc.tar.xz
 tar -xf binutils.tar.xz
 tar -xf gcc.tar.xz
+
+mv gcc-12.2.0 gcc
+mv binutils-2.40 bin
