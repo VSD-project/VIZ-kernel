133cccc283aa960762e6b7fd5d10f32640467f96
0xol <86998622+0xol@users.noreply.github.com>
2023-03-05 20:36:17 +1000

    removing toolchain.sh

---
+++ include/viz/stdint.h
@@ -0,0 +1,12 @@
+
+//VIZ is designed around LP64
+// long and ponters are 64 bit big
+
+typedef signed char     int8_t;
+typedef unsigned char   uint8_t;
+typedef short           int16_t;
+typedef unsigned short  uint16_t;
+typedef int             int32_t;
+typedef unsigned        uint32_t;
+typedef long            int64_t;
+typedef unsigned long   uint64_t;
